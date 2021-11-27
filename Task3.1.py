8e49e3795e474def33fc7c4e313432d26deaadf1
Manul <dmitriy-hak@yandex.ru>
2021-11-27 04:36:26 +0300

    Added tasks 3.1-3.6

--- Task3.1.py
+++ Task3.1.py
@@ -1,1 +1,10 @@
 def div(s_1, s_2):
+    try:
+        s_1, s_2 = int(s_1), int(s_2)
+        div_num = s_1 / s_2
+    except ValueError:
+        return 'Некорректные данные'
+    except ZeroDivisionError:
+        return 'Делить на ноль нельзя'
+
+print(div(input('Введите перое число - '), input('Введите второе число - ')))
